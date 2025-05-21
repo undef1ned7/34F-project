21934401e60ff292c81b13830e2e252cc19a69c9
undef1ned7 <myizambekov.baiel9@gmail.com>
2025-05-21 16:47:33 +0600

    initial

---
+++ main.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector(".mobile-btn");
+
+const toggleBurger = (menuClass, showClass) => {
+  let menu = document.querySelector(`.${menuClass}`);
+
+  if (menu.className.includes(showClass)) {
+    menu.className = menuClass;
+  } else {
+    menu.className += ` ${showClass}`;
+  }
+};
+
+btn.addEventListener("click", () => {
+  toggleBurger("header-nav", "header-nav-active");
+});
